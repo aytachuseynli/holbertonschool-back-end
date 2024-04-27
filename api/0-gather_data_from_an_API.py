36479d95995b37c59fd74774895851ff2cfc5a38
aytachuseynli <aytac_ha@mail.ru>
2024-04-27 23:16:18 +0400

    Updated:

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,40 +1,30 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""
+Module for task0 about request and API
+"""
+
 import requests
-import sys
+from sys import argv
 
 url_base = 'https://jsonplaceholder.typicode.com/users/'
 
 
-def get_employee_todo_progress(employee_id):
-    """
-    Fetches the TODO list progress for a given employee ID and
-    prints it to standard output.
-
-    Args:
-        employee_id (int): The ID of the employee whose
-        TODO list progress needs to be fetched.
-
-    Returns:
-        None
-    """
-    name = requests.get(url_base + str(employee_id)).json()
-    todos = requests.get(url_base + str(employee_id) + '/todos/').json()
+def get_data():
+    """ This function get data of the placeholders API """
+    name = requests.get(url_base + argv[1]).json()
+    todos = requests.get(url_base + argv[1] + '/todos/').json()
     count = 0
     title = ""
 
     for item in todos:
         if item['completed'] is True:
-            title += "\t{}\n".format(item['title'])
+            title += "\t {}\n".format(item['title'])
             count += 1
 
-    print("Employee {} is done with tasks({}/20):\n{}".format(
-        name['name'], count, title), end='')
+    print("Employee {} is done with tasks({}/20):\n{}".format(name['name'],
+                                                              count, title),
+          end='')
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py EMPLOYEE_ID")
-        sys.exit(1)
-
-    get_employee_todo_progress(int(sys.argv[1]))
+if __name__ == '__main__':
+    get_data()
