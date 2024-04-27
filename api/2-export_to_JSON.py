2c71f0c03dabf34c023b8630fd2b356cf075be98
aytachuseynli <aytac_ha@mail.ru>
2024-04-27 23:04:51 +0400

    Updated 2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -3,7 +3,8 @@
 Extends the Python script to export data in JSON format.
 Records all tasks that are owned by this employee.
 Format must be: { "USER_ID": [{"task": "TASK_TITLE",
-"completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, {"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]}
+"completed": TASK_COMPLETED_STATUS, "username": "USERNAME"},
+{"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]}
 File name must be: USER_ID.json
 """
 
